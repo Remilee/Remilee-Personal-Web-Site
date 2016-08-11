e2582e87db8f74bed5f438288c192ee14618b94a
Remilee White <katerinakreuz@gmail.com>
2016-08-11 16:56:55 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php	
 	$db_host = 'localhost';
 	$db_user = 'remilee';
-	$db_password = '3122';
+	$db_password = '***';
 	$db_name = 'diary';
 	$dbc = mysqli_connect($db_host, $db_user, $db_password, $db_name) or die('Ошибка подключения к mysql');
 	$utf8 = "SET NAMES `utf8`";
